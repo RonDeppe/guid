c8b836685905313e5d8217d6b3994f4945a9f997
Ron Deppe <97666744+RonDeppe@users.noreply.github.com>
2025-01-31 14:59:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace guid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(Guid.NewGuid());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
